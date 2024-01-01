f460c0eb6b73de4c7ecc424e164d05b15d39745c
Brendon Smith <bws@bws.bio>
2024-01-01 17:48:31 -0500

    Enable TFC GitHub Actions OIDC provisioning
    Credentials are required for the AWS Terraform provider so that
    Terraform can apply configurations. If using Terraform Cloud (TFC),
    credentials need to be provided there.
    
    One way to provide credentials for Terraform Cloud is by creating AWS
    IAM users, creating AWS access keys for the users, and storing the
    access keys in Terraform Cloud workspace variables. AWS access keys are
    persistent credentials that require manual rotation.
    
    Another way to provide credentials is through OpenID Connect (OIDC).
    OIDC allows Terraform runs to authenticate with AWS by assuming IAM
    roles that grant temporary security credentials, instead of by using
    persistent AWS access key credentials. Terraform Cloud added support for
    assuming roles with OIDC in March 2023.
    
    The previous commit implemented Terraform Cloud OIDC. This commit will
    enable the IAM role credentials obtained with OIDC in Terraform Cloud to
    provision GitHub Actions OIDC.
    
    After this commit, the GitHub Actions OIDC Terraform Cloud workspace
    will be updated with the necessary OIDC environment variables:
    
    - `TFC_AWS_PROVIDER_AUTH=true`
    - `TFC_AWS_RUN_ROLE_ARN=<AWS_IAM_ROLE_ARN>`
    
    The GitHub Actions OIDC Terraform Cloud workspace will then be able to
    assume a role and apply configurations.
    
    https://www.hashicorp.com/blog/dynamic-provider-credentials-now-ga-for-terraform-cloud
    https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials
    https://developer.hashicorp.com/terraform/tutorials/cloud/dynamic-credentials

--- aws/br3ndonland/terraform-cloud-oidc/main.tf
+++ aws/br3ndonland/terraform-cloud-oidc/main.tf
@@ -11,3 +11,106 @@ module "terraform_cloud_oidc" {
   source         = "./modules/terraform-cloud-oidc"
   tfc_workspaces = var.tfc_workspaces
 }
+
+data "aws_iam_policy_document" "github_actions_oidc_provisioning" {
+  statement {
+    actions = [
+      "iam:AddClientIDToOpenIDConnectProvider",
+      "iam:CreateOpenIDConnectProvider",
+      "iam:TagOpenIDConnectProvider",
+      "iam:UpdateOpenIDConnectProviderThumbprint",
+    ]
+    resources = ["arn:aws:iam::*:oidc-provider/token.actions.githubusercontent.com"]
+    sid       = "IAMOIDCProviderProvisioningActions"
+  }
+  statement {
+    actions = [
+      "iam:GetOpenIDConnectProvider",
+      "iam:ListOpenIDConnectProviders",
+      "iam:ListOpenIDConnectProviderTags",
+    ]
+    resources = ["*"]
+    sid       = "IAMOIDCProviderReadActions"
+  }
+  statement {
+    actions = [
+      "iam:DeleteOpenIDConnectProvider",
+      "iam:RemoveClientIDFromOpenIDConnectProvider",
+      "iam:UntagOpenIDConnectProvider",
+    ]
+    resources = ["arn:aws:iam::*:oidc-provider/token.actions.githubusercontent.com"]
+    sid       = "IAMOIDCProviderCleanupActions"
+  }
+  statement {
+    actions = [
+      "iam:CreatePolicy",
+      "iam:CreatePolicyVersion",
+    ]
+    resources = ["arn:aws:iam::*:policy/github*"]
+    sid       = "IAMPolicyProvisioningActions"
+  }
+  statement {
+    actions = [
+      "iam:GetPolicy",
+      "iam:GetPolicyVersion",
+      "iam:ListEntitiesForPolicy",
+      "iam:ListPolicies",
+      "iam:ListPolicyVersions",
+      "iam:ListUserPolicies",
+    ]
+    resources = ["*"]
+    sid       = "IAMPolicyReadActions"
+  }
+  statement {
+    actions   = ["iam:DeletePolicy", "iam:DeletePolicyVersion"]
+    resources = ["arn:aws:iam::*:policy/github*"]
+    sid       = "IAMPolicyCleanupActions"
+  }
+  statement {
+    actions = [
+      "iam:AttachRolePolicy",
+      "iam:CreateRole",
+      "iam:PutRolePolicy",
+      "iam:UpdateRole",
+      "iam:UpdateRoleDescription",
+      "iam:UpdateAssumeRolePolicy",
+    ]
+    resources = ["arn:aws:iam::*:role/github*"]
+    sid       = "IAMRoleProvisioningActions"
+  }
+  statement {
+    actions = [
+      "iam:GetRole",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListInstanceProfilesForRole",
+      "iam:ListRolePolicies",
+      "iam:ListRoles",
+    ]
+    resources = ["*"]
+    sid       = "IAMRoleReadActions"
+  }
+  statement {
+    actions = [
+      "iam:DeleteRole",
+      "iam:DeleteRolePolicy",
+      "iam:DetachRolePolicy",
+    ]
+    resources = ["arn:aws:iam::*:role/github*"]
+    sid       = "IAMRoleCleanupActions"
+  }
+}
+
+resource "aws_iam_policy" "github_actions_oidc_provisioning" {
+  description = "Allows provisioning of resources for GitHub Actions OIDC (OpenID Connect)."
+  name        = "github-actions-oidc-provisioning"
+  policy      = data.aws_iam_policy_document.github_actions_oidc_provisioning.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_oidc_provisioning" {
+  for_each = toset([
+    for key in keys(local.tfc_workspaces) :
+    key if strcontains(key, "github-actions-oidc")
+  ])
+  role       = module.terraform_cloud_oidc.aws_iam_roles[each.key].name
+  policy_arn = aws_iam_policy.github_actions_oidc_provisioning.arn
+}
