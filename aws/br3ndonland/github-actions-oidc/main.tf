17d408fadaf4732ab79d0b7681d966fb69e12330
Brendon Smith <bws@bws.bio>
2024-01-01 18:29:03 -0500

    Migrate GitHub Actions OIDC configurations
    Terraform configurations for GitHub Actions OIDC were originally in a
    separate GitHub repo. This commit will migrate the configurations to
    this repo.

---
+++ aws/br3ndonland/github-actions-oidc/main.tf
@@ -0,0 +1,39 @@
+locals {
+  github_repos = {
+    for repo in var.github_repos_with_oidc :
+    replace(repo, "/", "-") => {
+      owner = split("/", repo)[0]
+      repo  = split("/", repo)[1]
+    }
+  }
+}
+
+module "github_actions_oidc" {
+  source       = "br3ndonland/github-actions-oidc/aws"
+  github_repos = var.github_repos_with_oidc
+}
+
+data "aws_iam_policy_document" "s3_bucket_for_oidc" {
+  for_each = local.github_repos
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${each.value.repo}"]
+  }
+  statement {
+    actions   = ["s3:DeleteObject", "s3:GetObject", "s3:PutObject"]
+    resources = ["arn:aws:s3:::${each.value.repo}/*"]
+  }
+}
+
+resource "aws_iam_policy" "s3_bucket_for_oidc" {
+  for_each    = local.github_repos
+  name        = "github-actions-s3-${each.value.repo}"
+  description = "Allows access to a single S3 bucket with the given name"
+  policy      = data.aws_iam_policy_document.s3_bucket_for_oidc[each.key].json
+}
+
+resource "aws_iam_role_policy_attachment" "s3_bucket_for_oidc" {
+  for_each   = aws_iam_policy.s3_bucket_for_oidc
+  role       = module.github_actions_oidc.aws_iam_roles[each.key].name
+  policy_arn = each.value.arn
+}
---
+++ aws/br3ndonland/github-actions-oidc/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_provider_region" {
+  description = "AWS region to use when configuring AWS Terraform provider"
+  type        = string
+}
+
+variable "github_repos_with_oidc" {
+  description = "Set of GitHub repositories for which to configure GitHub Actions OpenID Connect (OIDC), in owner/repo format"
+  type        = set(string)
+}
