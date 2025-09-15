1e402778f2b94ec3d8526621ffa8e77ea2de29b4
Brendon Smith <bws@bws.bio>
2025-09-15 18:59:34 -0400

    Switch to `versions.tofu`
    This commit will move provider and language versions from `versions.tf`
    to `versions.tofu`. The `versions.tofu` file helps ensure that OpenTofu
    is used consistently instead of HashiCorp Terraform. OpenTofu will give
    preference to the `versions.tofu` file over the `versions.tf` file, but
    HashiCorp Terraform will read `versions.tf` and error when it can't find
    the required version. The idea is to enforce use of OpenTofu by raising
    an error if Terraform is used instead.
    
    https://opentofu.org/docs/language/files/

--- aws/br3ndonland/github-actions-oidc/versions.tf
+++ aws/br3ndonland/github-actions-oidc/versions.tf
@@ -1,9 +1,10 @@
+/*
+  This file helps ensure that OpenTofu is used consistently instead of HashiCorp Terraform.
+  OpenTofu will give preference to the `versions.tofu` file over the `versions.tf` file, but
+  HashiCorp Terraform will read `versions.tf` and error when it can't find the required version.
+  The idea is to enforce use of OpenTofu by raising an error if Terraform is used instead.
+  https://opentofu.org/docs/language/files/
+*/
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0, < 6.0"
-    }
-  }
-  required_version = ">= 1.8, < 2"
+  required_version = "> 99"
 }
--- github/br3ndonland/versions.tf
+++ github/br3ndonland/versions.tf
@@ -1,13 +1,10 @@
+/*
+  This file helps ensure that OpenTofu is used consistently instead of HashiCorp Terraform.
+  OpenTofu will give preference to the `versions.tofu` file over the `versions.tf` file, but
+  HashiCorp Terraform will read `versions.tf` and error when it can't find the required version.
+  The idea is to enforce use of OpenTofu by raising an error if Terraform is used instead.
+  https://opentofu.org/docs/language/files/
+*/
 terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = ">= 5, < 6"
-    }
-    onepassword = {
-      source  = "1Password/onepassword"
-      version = ">= 2, < 3"
-    }
-  }
-  required_version = ">= 1.8, < 2"
+  required_version = "> 99"
 }
