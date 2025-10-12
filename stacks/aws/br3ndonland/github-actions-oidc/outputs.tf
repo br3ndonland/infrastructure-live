a1ea281c76f2c27c8befd96870360600d7fa7e01
Brendon Smith <bws@bws.bio>
2025-10-11 21:04:14 -0400

    Simplify IAM policy output
    The `github-actions-oidc-provisioning` IAM policy was previously output
    so it could be used in another stack (the `terraform-cloud-oidc` stack).
    Although that stack is no longer in use, the output will be maintained
    in case it is useful to other stacks in the future.
    
    The `attachment_count` attribute of `aws_iam_policy` may change outside
    of this stack, and may therefore induce drift in the output. This commit
    will omit the `attachment_count` from the output to avoid this drift.

--- stacks/aws/br3ndonland/github-actions-oidc/outputs.tf
+++ stacks/aws/br3ndonland/github-actions-oidc/outputs.tf
@@ -1,3 +1,6 @@
 output "github_actions_oidc_provisioning_policy" {
-  value = aws_iam_policy.github_actions_oidc_provisioning
+  value = {
+    for key, value in aws_iam_policy.github_actions_oidc_provisioning :
+    key => value if key != "attachment_count"
+  }
 }
