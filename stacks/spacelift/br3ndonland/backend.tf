b29ee0a078cf32148d389c984f3313b8f3f581be
Brendon Smith <bws@bws.bio>
2025-10-14 23:34:51 -0400

    Move Spacelift stack up one directory level
    There will only be one Spacelift admin stack per Spacelift organization,
    so there is no need for the extra directory level.

--- stacks/spacelift/br3ndonland/backend.tf
+++ stacks/spacelift/br3ndonland/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = var.s3_backend_bucket
-    key          = "stacks/spacelift/br3ndonland/spacelift-admin/terraform.tfstate"
+    key          = "stacks/spacelift/br3ndonland/terraform.tfstate"
     region       = "us-east-2"
     use_lockfile = true
   }
