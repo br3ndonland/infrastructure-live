e0574a6c8e99c8d1f76ba76f4038a2f24488eaf7
Brendon Smith <bws@bws.bio>
2025-10-31 18:20:32 -0400

    Don't attempt to provision Spacelift root space
    The root space is provisioned automatically.

--- stacks/spacelift/br3ndonland/spacelift_space.tf
+++ stacks/spacelift/br3ndonland/spacelift_space.tf
@@ -1,5 +1,8 @@
 resource "spacelift_space" "in_root_space" {
-  for_each         = data.terraform_remote_state.spacelift_oidc.outputs.aws_iam_roles_for_spacelift_oidc
+  for_each = {
+    for key, value in data.terraform_remote_state.spacelift_oidc.outputs.aws_iam_roles_for_spacelift_oidc :
+    key => value if value.spacelift_space != "root"
+  }
   inherit_entities = false
   name             = each.value.spacelift_space
   parent_space_id  = "root"
